610517427d87c24d7aa757e4efb011c5f3e08d26
MarcinJuraszek <mail@marcinjuraszek.com>
2014-06-21 11:46:09 -0700

    UT Refactor - primitive, nullable and assert helper

---
+++ src/CloneExtensions.UnitTests/Helpers/AssertHelpers.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace CloneExtensions.UnitTests.Helpers
+{
+    class AssertHelpers
+    {
+        public static void GetCloneAndAssert<T>(Func<T> getInstance, bool assertSame = false)
+        {
+            var source = getInstance();
+            var target = CloneFactory.GetClone(source);
+
+            if (source == null)
+            {
+                Assert.IsNull(target);
+            }
+            else
+            {
+                Assert.AreEqual(source, target);
+                if (assertSame)
+                {
+                    Assert.AreSame(source, target);
+                }
+            }
+        }
+    }
+}
