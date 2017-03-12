f41a871b3bb22b47c6d84795791e01b9f371b3af
Marcin Juraszek <mail@marcinjuraszek.com>
2017-03-12 16:38:58 -0700

    net4;net45;net461;netcoreapp1.1

--- src/CloneExtensions/TypeExtensions.cs
+++ src/CloneExtensions/TypeExtensions.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Linq;
+using System.Reflection;
 
 namespace CloneExtensions
 {
@@ -7,7 +8,87 @@ namespace CloneExtensions
     {
         public static bool IsPrimitiveOrKnownImmutable(this Type type)
         {
-            return type.IsPrimitive || CloneFactory.KnownImmutableTypes.Contains(type);
+            return type.IsPrimitive() || CloneFactory.KnownImmutableTypes.Contains(type);
         }
+
+#if NET40 || NET45 || NET461
+        public static bool IsAbstract(this Type type)
+        {
+            return type.IsAbstract;
+        }
+        
+        public static bool IsPrimitive(this Type type)
+        {
+            return type.IsPrimitive;
+        }
+
+        public static bool IsGenericType(this Type type)
+        {
+            return type.IsGenericType;
+        }
+
+        public static bool IsValueType(this Type type)
+        {
+            return type.IsValueType;
+        }
+
+        public static bool IsInterface(this Type type)
+        {
+            return type.IsInterface;
+        }
+#else
+
+        public static ConstructorInfo[] GetConstructors(this Type type)
+        {
+            return type.GetTypeInfo().GetConstructors();
+        }
+
+        public static ConstructorInfo GetConstructor(this Type type, params Type[] types)
+        {
+            return type.GetTypeInfo().GetConstructor(types);
+        }
+
+        public static Type[] GetInterfaces(this Type type)
+        {
+            return type.GetTypeInfo().GetInterfaces();
+        }
+
+        public static bool IsAssignableFrom(this Type type, Type c)
+        {
+            return type.GetTypeInfo().IsAssignableFrom(c);
+        }
+
+        public static Type[] GetGenericArguments(this Type type)
+        {
+            return type.GetTypeInfo().IsGenericTypeDefinition
+                ? type.GetTypeInfo().GenericTypeParameters
+                : type.GetTypeInfo().GenericTypeArguments;
+        }
+
+        public static bool IsAbstract(this Type type)
+        {
+            return type.GetTypeInfo().IsAbstract;
+        }
+
+        public static bool IsPrimitive(this Type type)
+        {
+            return type.GetTypeInfo().IsPrimitive;
+        }
+
+        public static bool IsGenericType(this Type type)
+        {
+            return type.GetTypeInfo().IsGenericType;
+        }
+
+        public static bool IsValueType(this Type type)
+        {
+            return type.GetTypeInfo().IsValueType;
+        }
+
+        public static bool IsInterface(this Type type)
+        {
+            return type.GetTypeInfo().IsInterface;
+        }
+#endif
     }
 }
