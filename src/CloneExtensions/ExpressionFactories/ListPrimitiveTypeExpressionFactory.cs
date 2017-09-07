5984423d77601549c1faaf3ca2e9427c35107a86
deipax <deipax@outlook.com>
2017-09-07 14:07:51 -0700

    Supply a Expression factory for a List of primitives.
    Similar to the Array of primitives, there is no need to call the clone method for primitivies.  Since List is so common, providing a custom Expression factory should be quite useful.  According to my testing, here are the performance differences:
    
    List of Strings:   22,815 ops per second ->1,111,000 (about 4700% increase)
    List of Bytes:  46,153 ops per second ->1,199,880 (about 2500% increase)
    List of Ints: 44,863 ops per second -> 990,000 (about 2100% increase)
    List of Delegates: 1,119,194 ops per second ->5,221,409 (about 365% increase)
    List of DateTime: 1,274,697 ops per second -> 4,629,166 (about 263% increase)
    List of TimeSpan: 1,251,564 ops per second -> 5,221,409 (about 317% increase)
    
    One thing I had to do in order to address an assertion made by the unit tests was to not clone the element is the Flag.CollectionItems was missing.  Which I found surpising, should there be such an assertion of Arrays of items as well?  Currently the Array expression factorys do not check flags at all.

---
+++ src/CloneExtensions/ExpressionFactories/ListPrimitiveTypeExpressionFactory.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Reflection;
+
+namespace CloneExtensions.ExpressionFactories
+{
+    class ListPrimitiveTypeExpressionFactory<T> : DeepShallowExpressionFactoryBase<T>
+    {
+        private static Type _type = typeof(T);
+        private static ConstructorInfo _constructor = _type
+            .GetConstructors()
+            .Where(x =>
+                x.GetParameters().Length == 1 &&
+                x.GetParameters().ElementAt(0).ParameterType.IsGenericType())
+            .FirstOrDefault();
+
+        private static ConstructorInfo _capacityContructor = _type
+            .GetConstructors()
+            .Where(x =>
+                x.GetParameters().Length == 1 &&
+                x.GetParameters().ElementAt(0).ParameterType == typeof(int))
+            .FirstOrDefault();
+
+        public ListPrimitiveTypeExpressionFactory(
+            ParameterExpression source,
+            Expression target,
+            ParameterExpression flags,
+            ParameterExpression initializers,
+            ParameterExpression clonedObjects)
+            : base(source, target, flags, initializers, clonedObjects)
+        {
+        }
+
+        public override bool AddNullCheck
+        {
+            get { return true; }
+        }
+
+        public override bool VerifyIfAlreadyClonedByReference
+        {
+            get { return true; }
+        }
+
+        protected override Expression GetCloneExpression(Func<Type, Expression, Expression> getItemCloneExpression)
+        {
+            var ifThenElse = Expression.IfThenElse(
+                Helpers.GetCloningFlagsExpression(CloningFlags.CollectionItems, Flags),
+                Expression.Assign(Target, Expression.New(_constructor, Source)),
+                Expression.Assign(Target, Expression.New(_capacityContructor, Expression.Property(Source, "Count"))));
+
+            return Expression.Block(
+                ifThenElse,
+                GetAddToClonedObjectsExpression());
+        }
+    }
+}
